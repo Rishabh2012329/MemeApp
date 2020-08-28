e5f1385470b1a73392bd1af0f16c16486bbcb373
unknown <Rishabh>
2020-08-28 12:06:58 +0530

    Updates

---
+++ precache-manifest.f3eda98e4fdb8bca13db26595280e8ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d7e96af0891c10b1466e253b2b91af",
+    "url": "/Rishabh2012329/memeApp/index.html"
+  },
+  {
+    "revision": "124ec322ae2e7624dfb0",
+    "url": "/Rishabh2012329/memeApp/static/css/main.4ef87527.chunk.css"
+  },
+  {
+    "revision": "ec651ffd4de4231ed717",
+    "url": "/Rishabh2012329/memeApp/static/js/2.f6f4488a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Rishabh2012329/memeApp/static/js/2.f6f4488a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "124ec322ae2e7624dfb0",
+    "url": "/Rishabh2012329/memeApp/static/js/main.5c3d22ae.chunk.js"
+  },
+  {
+    "revision": "8bcb906fe31ae86c154c",
+    "url": "/Rishabh2012329/memeApp/static/js/runtime-main.d80fd2ae.js"
+  }
+]);
